4ddadfce522751c24b031f995a6ad4b085f189ff
Finn <tupfi2@gmx.de>
2020-02-20 18:29:59 +0100

    Fixed aixformation hero

--- lib/aixformation/aixformation_page.dart
+++ lib/aixformation/aixformation_page.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:viktoriaapp/aixformation/aixformation_row.dart';
 import 'package:viktoriaapp/app/app_page.dart';
 import 'package:viktoriaapp/models/models.dart';
-import 'package:viktoriaapp/plugins/platform/platform.dart';
 import 'package:viktoriaapp/utils/app_bar.dart';
 import 'package:viktoriaapp/utils/bottom_navigation.dart';
+import 'package:viktoriaapp/utils/custom_hero.dart';
 import 'package:viktoriaapp/utils/empty_list.dart';
 import 'package:viktoriaapp/utils/size_limit.dart';
 import 'package:viktoriaapp/utils/static.dart';
@@ -21,43 +21,39 @@ class AiXformationPage extends StatelessWidget {
   Widget build(BuildContext context) => Column(
         children: <Widget>[
           Expanded(
-            child: Hero(
-              tag: !Platform().isWeb ? Keys.aiXformation : hashCode,
-              child: Material(
-                type: MaterialType.transparency,
-                child: CustomScrollView(
-                  slivers: [
-                    CustomAppBar(
-                      title: page.title,
-                      actions: page.actions,
-                      sliver: true,
-                    ),
-                    SliverList(
-                      delegate: SliverChildListDelegate(
-                        Static.aiXformation.data.posts.isNotEmpty
-                            ? Static.aiXformation.data.posts
-                                .map((post) => Center(
-                                      child: SizeLimit(
-                                        child: Container(
-                                          margin: EdgeInsets.all(10),
-                                          child: AiXformationRow(
-                                            post: post,
-                                          ),
-                                        ),
-                                      ),
-                                    ))
-                                .toList()
-                                .cast<Widget>()
-                            : [
-                                Center(
-                                  child: EmptyList(title: 'Keine Artikel'),
-                                )
-                              ],
-                      ),
-                    ),
-                  ],
+            child: CustomScrollView(
+              slivers: [
+                CustomAppBar(
+                  title: page.title,
+                  actions: page.actions,
+                  sliver: true,
                 ),
-              ),
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    Static.aiXformation.data.posts.isNotEmpty
+                        ? Static.aiXformation.data.posts
+                            .map((post) => Center(
+                                  child: SizeLimit(
+                                    child: Container(
+                                      margin: EdgeInsets.all(10),
+                                      child: AiXformationRow(
+                                        post: post,
+                                      ),
+                                    ),
+                                  ),
+                                ))
+                            .toList()
+                            .cast<Widget>()
+                        : [
+                            Center(
+                              child: EmptyList(title: 'Keine Artikel'),
+                            )
+                          ],
+                  ),
+                ),
+              ],
+            ),
+          ),
           CustomHero(
             tag: Keys.navigation(Keys.aiXformation),
             child: Material(
