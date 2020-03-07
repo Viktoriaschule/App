e106ed9b846ce98fddb4dcebdedeaa7948bb8b48
jld3103 <jld3103yt@gmail.com>
2020-03-07 09:30:30 +0100

    Fix settings scroll

--- frame/lib/settings/settings_page.dart
+++ frame/lib/settings/settings_page.dart
@@ -45,122 +45,113 @@ class _SettingsPageState extends Interactor<SettingsPage> {
         title: Pages.of(context).pages[Keys.settings].title,
         loadingKeys: [Keys.tags],
       ),
-      body: Material(
-        child: Center(
-          child: SizeLimit(
-            child: Scrollbar(
-              child: ListView(
-                shrinkWrap: true,
-                padding: EdgeInsets.all(5),
-                children: [
-                  if (notificationFeatures.isNotEmpty)
-                    ListGroup(
-                      title: 'Benachrichtigungen',
-                      children: notificationFeatures
-                          .map(
-                            (feature) => CheckboxListTile(
-                              title: Text(
-                                feature.name,
-                                style: TextStyle(
-                                  color: ThemeWidget.of(context).textColor,
-                                ),
-                              ),
-                              checkColor: lightColor,
-                              activeColor: Theme.of(context).accentColor,
-                              value: getNotifications(feature.featureKey),
-                              onChanged: (value) async {
-                                setState(() {
-                                  Static.storage.setBool(
-                                      Keys.notifications(feature.featureKey),
-                                      value);
-                                });
-                                try {
-                                  await Static.tags.syncDevice(context);
-                                  // ignore: empty_catches
-                                } on DioError {}
-                              },
-                            ),
-                          )
-                          .toList(),
-                    ),
-                  ListGroup(
-                    title: 'Design',
-                    children: [
-                      CheckboxListTile(
+      body: Scrollbar(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.all(5),
+          children: [
+            if (notificationFeatures.isNotEmpty)
+              ListGroup(
+                title: 'Benachrichtigungen',
+                children: notificationFeatures
+                    .map(
+                      (feature) => CheckboxListTile(
                         title: Text(
-                          'Automatisch',
+                          feature.name,
                           style: TextStyle(
                             color: ThemeWidget.of(context).textColor,
                           ),
                         ),
                         checkColor: lightColor,
                         activeColor: Theme.of(context).accentColor,
-                        value: _automaticDesign,
+                        value: getNotifications(feature.featureKey),
                         onChanged: (value) async {
                           setState(() {
-                            _automaticDesign = value;
-                            Static.storage.setBool(Keys.automaticDesign, value);
-                            EventBus.of(context).publish(ThemeChangedEvent());
+                            Static.storage.setBool(
+                                Keys.notifications(feature.featureKey), value);
                           });
+                          try {
+                            await Static.tags.syncDevice(context);
+                            // ignore: empty_catches
+                          } on DioError {}
                         },
                       ),
-                      CheckboxListTile(
-                        title: Text(
-                          'Dunkles Design',
-                          style: TextStyle(
-                            color: ThemeWidget.of(context).textColor,
-                          ),
-                        ),
-                        checkColor: lightColor,
-                        activeColor: Theme.of(context).accentColor,
-                        value: _darkMode,
-                        onChanged: !_automaticDesign
-                            ? (value) async {
-                                setState(() {
-                                  _darkMode = value;
-                                  Static.storage.setBool(Keys.darkMode, value);
-                                  EventBus.of(context)
-                                      .publish(ThemeChangedEvent());
-                                });
-                              }
-                            : null,
-                      ),
-                    ],
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 20, left: 5, right: 5),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: CustomButton(
-                        onPressed: () {
-                          Static.user.clear();
-                          Static.tags.clear();
-                          Static.updates.clear();
-                          // Clear the data of all features
-                          FeaturesWidget.of(context)
-                              .features
-                              .forEach((f) => f.loader.clear());
-                          Static.subjects.clear();
-                          Static.storage
-                              .getKeys()
-                              .forEach(Static.storage.remove);
-                          EventBus.of(context).publish(ThemeChangedEvent());
-                          Navigator.of(context).pushNamedAndRemoveUntil(
-                              '/${Keys.login}', (r) => false);
-                        },
-                        child: Text(
-                          'Abmelden',
-                          style: TextStyle(
-                            color: darkColor,
-                          ),
-                        ),
-                      ),
+                    )
+                    .toList(),
+              ),
+            ListGroup(
+              title: 'Design',
+              children: [
+                CheckboxListTile(
+                  title: Text(
+                    'Automatisch',
+                    style: TextStyle(
+                      color: ThemeWidget.of(context).textColor,
                     ),
                   ),
-                ],
+                  checkColor: lightColor,
+                  activeColor: Theme.of(context).accentColor,
+                  value: _automaticDesign,
+                  onChanged: (value) async {
+                    setState(() {
+                      _automaticDesign = value;
+                      Static.storage.setBool(Keys.automaticDesign, value);
+                      EventBus.of(context).publish(ThemeChangedEvent());
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: Text(
+                    'Dunkles Design',
+                    style: TextStyle(
+                      color: ThemeWidget.of(context).textColor,
+                    ),
+                  ),
+                  checkColor: lightColor,
+                  activeColor: Theme.of(context).accentColor,
+                  value: _darkMode,
+                  onChanged: !_automaticDesign
+                      ? (value) async {
+                          setState(() {
+                            _darkMode = value;
+                            Static.storage.setBool(Keys.darkMode, value);
+                            EventBus.of(context).publish(ThemeChangedEvent());
+                          });
+                        }
+                      : null,
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 5, left: 10, right: 10),
+              child: SizedBox(
+                width: double.infinity,
+                child: CustomButton(
+                  onPressed: () {
+                    Static.user.clear();
+                    Static.tags.clear();
+                    Static.updates.clear();
+                    // Clear the data of all features
+                    FeaturesWidget.of(context)
+                        .features
+                        .where((f) => f.loader != null)
+                        .forEach((f) => f.loader.clear());
+                    Static.subjects.clear();
+                    Static.storage.getKeys().forEach(Static.storage.remove);
+                    EventBus.of(context).publish(ThemeChangedEvent());
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                        '/${Keys.login}', (r) => false);
+                  },
+                  child: Text(
+                    'Abmelden',
+                    style: TextStyle(
+                      color: darkColor,
+                    ),
+                  ),
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
