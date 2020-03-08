56cc0e8d8e16b856cb87447a2c6021d5646836ee
Finn <tupfi2@gmx.de>
2020-03-08 12:22:40 +0100

    Remove feature specific tags code in the utils

--- features/cafetoria/lib/src/cafetoria_tags.dart
+++ features/cafetoria/lib/src/cafetoria_tags.dart
@@ -9,61 +9,92 @@ import 'cafetoria_events.dart';
 class CafetoriaTagsHandler extends TagsHandler {
   @override
   void syncFromServer(Tags tags, BuildContext context) {
+    final cafetoriaLogin = CafetoriaTags.fromJson(tags.data['cafetoria']);
     bool cafetoriaIsNewer = false;
     final String cafetoriaModified =
         Static.storage.getString(CafetoriaKeys.cafetoriaModified);
     if (cafetoriaModified != null) {
       final DateTime local = DateTime.parse(cafetoriaModified);
-      if (tags.cafetoriaLogin.timestamp.isAfter(local)) {
+      if (cafetoriaLogin.timestamp.isAfter(local)) {
         cafetoriaIsNewer = true;
       }
-    } else if (tags.cafetoriaLogin.id != null) {
+    } else if (cafetoriaLogin.id != null) {
       cafetoriaIsNewer = true;
     }
 
     if (cafetoriaIsNewer) {
-      if (tags.cafetoriaLogin.id == null) {
+      if (cafetoriaLogin.id == null) {
         Static.storage.setString(CafetoriaKeys.cafetoriaId, null);
         Static.storage.setString(CafetoriaKeys.cafetoriaPassword, null);
       } else {
-        final String decryptedID = decryptText(tags.cafetoriaLogin.id);
-        final String decryptedPassword =
-            decryptText(tags.cafetoriaLogin.password);
+        final String decryptedID = decryptText(cafetoriaLogin.id);
+        final String decryptedPassword = decryptText(cafetoriaLogin.password);
         Static.storage.setString(CafetoriaKeys.cafetoriaId, decryptedID);
         Static.storage
             .setString(CafetoriaKeys.cafetoriaPassword, decryptedPassword);
       }
       Static.storage.setString(CafetoriaKeys.cafetoriaModified,
-          tags.cafetoriaLogin.timestamp.toIso8601String());
+          cafetoriaLogin.timestamp.toIso8601String());
       EventBus.of(context).publish(CafetoriaUpdateEvent());
     }
   }
 
   @override
   Map<String, dynamic> syncToServer(Tags tags) {
+    final cafetoriaLogin = CafetoriaTags.fromJson(tags.data['cafetoria']);
     final Map<String, dynamic> tagsToUpdate = {};
 
     final String id = Static.storage.getString(CafetoriaKeys.cafetoriaId);
     final String password =
-        Static.storage.getString(CafetoriaKeys.cafetoriaPassword);
+    Static.storage.getString(CafetoriaKeys.cafetoriaPassword);
     final String lastModified =
-        Static.storage.getString(CafetoriaKeys.cafetoriaModified);
+    Static.storage.getString(CafetoriaKeys.cafetoriaModified);
 
     // If the local cafetoria login data is set and newer than the server login data
     if (lastModified != null &&
-        DateTime.parse(lastModified).isAfter(tags.cafetoriaLogin.timestamp)) {
+        DateTime.parse(lastModified).isAfter(cafetoriaLogin.timestamp)) {
       final encryptedId = id == null ? null : encryptText(id);
       final encryptedPassword = password == null ? null : encryptText(password);
 
-      if (tags.cafetoriaLogin.id != encryptedId ||
-          tags.cafetoriaLogin.password != encryptedPassword) {
+      if (cafetoriaLogin.id != encryptedId ||
+          cafetoriaLogin.password != encryptedPassword) {
         tagsToUpdate['cafetoria'] = CafetoriaTags(
-                id: encryptedId,
-                password: encryptedPassword,
-                timestamp: DateTime.parse(lastModified))
+            id: encryptedId,
+            password: encryptedPassword,
+            timestamp: DateTime.parse(lastModified))
             .toMap();
       }
     }
     return tagsToUpdate;
   }
+}
+
+/// Describes the cafetoria tags
+class CafetoriaTags {
+  // ignore: public_member_api_docs
+  CafetoriaTags({this.id, this.password, this.timestamp});
+
+  /// Creates cafetoria tags from json map
+  factory CafetoriaTags.fromJson(Map<String, dynamic> json) =>
+      CafetoriaTags(
+          id: json['id'],
+          password: json['password'],
+          timestamp: DateTime.parse(json['timestamp']));
+
+  // ignore: public_member_api_docs
+  final String id;
+
+  // ignore: public_member_api_docs
+  final String password;
+
+  /// Last updated timestamp
+  final DateTime timestamp;
+
+  /// Converts cafetoria tags to json map
+  Map<String, dynamic> toMap() =>
+      {
+        'id': id,
+        'password': password,
+        'timestamp': timestamp.toIso8601String()
+      };
 }
